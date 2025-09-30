2eb3b7ddc74983a11bee13ce5b4e365e4c4b036c
yasin7a <arafath7yasin@gmail.com>
2025-09-30 14:22:22 +0600

    new

---
+++ medusa-config.ts
@@ -0,0 +1,17 @@
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+
+module.exports = defineConfig({
+  projectConfig: {
+    databaseName: process.env.DATABASE_NAME,
+    databaseUrl: process.env.DATABASE_URL,
+    http: {
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
+  }
+})
