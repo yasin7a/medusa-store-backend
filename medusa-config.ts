8612b971ed0bf7003735bd655ac4f18ff611e3f5
yasin7a <arafath7yasin@gmail.com>
2025-09-30 16:44:40 +0600

    htyu

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,8 +1,11 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
+  admin: {
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true" || false,
+  },
   projectConfig: {
     databaseName: process.env.DATABASE_NAME,
     databaseUrl: process.env.DATABASE_URL,
@@ -12,6 +15,6 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
-})
+    },
+  },
+});
